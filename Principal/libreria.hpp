44639a404794d547b9b21a17ea6986acbc96c4c8
Luisnerjch <luisnerjch@gmail.com>
2025-10-25 19:42:27 -0500

    Condicionales y menú de selección

--- Principal/libreria.hpp
+++ Principal/libreria.hpp
@@ -4,10 +4,13 @@
 #include <iostream>
 using namespace std;
 
-string listaRutas[6] = {"A102", "A105", "T102", "T100E", "X102", "X103"};
+//VARIABLES GLOBALES
+int opcion;
+int sentido;
+string listaRutas[5] = {"A101", "A102", "T102", "X102", "X103"};
 
 struct Rutas {
-    string A102[6] = {"Colegio Castillo", "U.San Buenaventura", "C.C. San Fernando", "Entrada Simon Bolivar", "Tenaris", "UTB"};
+    string A102[11];
     string X102[6];
     string T102[6];
     string T100E[6];
@@ -20,23 +23,27 @@ void bienvenida() {
     cout << "       Bienvenido a EscalApp\n";
     cout << "=====================================\n\n";
 }
-int opcion;
 
 void seleccionarRuta() {
     cout << "===== Que ruta tomaras hoy? =====" << endl << endl;
 
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 5; i++) {
         cout << i + 1 << ". " << listaRutas[i] << endl;
     }
-    cout << "\nSelecciona una opcion (1-6): ";
+    cout << "\nSelecciona una opcion (1-5): ";
     cin >> opcion;
 
-    if (opcion < 1 or opcion > 6) {
-        cout << "Opcion invalida. Por favor, selecciona una opcion entre 1 y 6." << endl;
+    if (opcion < 1 or opcion > 5) {
+        cout << "Opcion invalida. Por favor, selecciona una opcion entre 1 y 5." << endl;
     } 
     else {
         cout << "Has seleccionado la ruta: " << listaRutas[opcion - 1] << endl << endl;
     }
+}
+
+void seleccionarSentido() {
+    cout << "1. IDA\n" << "2. VUELTA\n" << "SELECCIONE EL SENTIDO: ";
+    cin >> sentido;
 }
 
 #endif
--- Principal/main.cpp
+++ Principal/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include "libreria.hpp"
+#include <string>
 using namespace std;
 
 int main() {
@@ -12,11 +13,111 @@ int main() {
   Rutas ra102;
   
 
-  if (opcion == 1) {
-    cout << "Ruta A102:" << endl;
-        for (int i = 0; i < 6; i++) {
-    cout << " > Parada " << i + 1 << ": " << ra102.A102[i] << endl; 
-  }
+
+  
+
+  switch (opcion) {
+    case 1:
+        cout << "RUTA A101" << endl;
+        seleccionarSentido();
+
+        if (sentido == 1) {
+          cout << endl;
+          ifstream a101_ida("../Rutas/rutas/A101-Ida.txt");
+          string linea;
+          while (getline(a101_ida, linea)) {
+            cout << linea << endl;
+          } a101_ida.close();
+          
+        }
+        if (sentido == 2) {
+          cout << endl;
+          ifstream a101_vuelta("../Rutas/rutas/A101-vuelta.txt");
+          string linea;
+          while (getline(a101_vuelta, linea)) {
+            cout << linea << endl;
+          } a101_vuelta.close();
+        }
+    case 2:
+        cout << "RUTA A102" << endl;
+        seleccionarSentido();
+
+        if (sentido == 1) {
+          cout << endl;
+          ifstream a102_ida("../Rutas/rutas/A102-Ida.txt");
+          string linea;
+          while (getline(a102_ida, linea)) {
+            cout << linea << endl;
+          } a102_ida.close();
+        }
+        if (sentido == 2) {
+          cout << endl;
+          ifstream a102_vuelta("../Rutas/rutas/A102-vuelta.txt");
+          string linea;
+          while (getline(a102_vuelta, linea)) {
+            cout << linea << endl;
+          } a102_vuelta.close();
+        }
+    case 3:
+        cout << "RUTA T102" << endl;
+        seleccionarSentido();
+
+        if (sentido == 1) {
+          cout << endl;
+          ifstream t102_ida("../Rutas/rutas/T102-Ida.txt");
+          string linea;
+          while (getline(t102_ida, linea)) {
+            cout << linea << endl;
+          } t102_ida.close();
+        }
+        if (sentido == 2) {
+          cout << endl;
+          ifstream t102_vuelta("../Rutas/rutas/T102-vuelta.txt");
+          string linea;
+          while (getline(t102_vuelta, linea)) {
+            cout << linea << endl;
+          } t102_vuelta.close();
+        }
+    case 4:
+        cout << "RUTA X102" << endl;
+        seleccionarSentido();
+
+        if (sentido == 1) {
+          cout << endl;
+          ifstream x102_ida("../Rutas/rutas/X102-Ida.txt");
+          string linea;
+          while (getline(x102_ida, linea)) {
+            cout << linea << endl;
+          } x102_ida.close();
+        }
+        if (sentido == 2) {
+          cout << endl;
+          ifstream x102_vuelta("../Rutas/rutas/X102-vuelta.txt");
+          string linea;
+          while (getline(x102_vuelta, linea)) {
+            cout << linea << endl;
+          } x102_vuelta.close();
+        }
+    case 5: 
+        cout << "RUTA X103" << endl;
+        seleccionarSentido();
+
+        if (sentido == 1) {
+          cout << endl;
+          ifstream x103_ida("../Rutas/rutas/X103-Ida.txt");
+          string linea;
+          while (getline(x103_ida, linea)) {
+            cout << linea << endl;
+          } x103_ida.close();
+        }
+        if (sentido == 2) {
+          cout << endl;
+          ifstream x103_vuelta("../Rutas/rutas/X103-vuelta.txt");
+          string linea;
+          while (getline(x103_vuelta, linea)) {
+            cout << linea << endl;
+          } x103_vuelta.close();
+        }
     }
 
 
