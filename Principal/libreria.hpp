abce0ca23b0eb1e52308b63da836f9aef81965ec
Luisnerjch <luisnerjch@gmail.com>
2025-10-27 01:35:53 -0500

    Agregué comentarios, hice cambios en algunas funciones en caso de que el usuario ingrese datos inválidos

--- Principal/libreria.hpp
+++ Principal/libreria.hpp
@@ -11,16 +11,20 @@ using namespace std;
 //VARIABLES GLOBALES
 int opcion;
 int sentido;
+int nueva_opcion;
 string parada;
+string linea;
 string listaRutas[5] = {"A101", "A102", "T102", "X102", "X103"};
 
-void bienvenida() {
+
+//FUNCIONES
+void bienvenida() { //FUNCION DE BIENVENIDA Y PARA INICIAR LA APLICACION
     cout << "=====================================\n";
     cout << "       Bienvenido a EscalApp\n";
     cout << "=====================================\n\n";
 }
 
-void barraCarga() {
+void barraCarga() { //FUNCION DE ANIMACION DE CARGA
     cout << "\nIniciando aplicacion";
     for (int i = 0; i < 5; i++) {
         cout << ".";
@@ -29,37 +33,54 @@ void barraCarga() {
     cout << "\nAplicacion iniciada!\n" << endl;
 }
 
-void seleccionarRuta() {
-    cout << "===== Que ruta tomaras hoy? =====" << endl << endl;
+void seleccionarRuta() { // función para seleccionar la ruta que el usuario tomará
+        cout << "===== Que ruta tomaras hoy? =====" << endl << endl;
 
-    for (int i = 0; i < 5; i++) {
-        cout << i + 1 << ". " << listaRutas[i] << endl;
-    }
-    cout << "\nSelecciona una opcion (1-5): ";
-    cin >> opcion;
+        for (int i = 0; i < 5; i++) {
+            cout << i + 1 << ". " << listaRutas[i] << endl;
+        }
+        cout << "\nSelecciona una opcion (1-5): ";
+        cin >> opcion;
+        
+        if (opcion >= 1 and opcion <= 5) {
+            cout << "Has seleccionado la ruta: " << listaRutas[opcion - 1] << endl << endl;
+        } else {
+            cout << "Opcion invalida. Ingrese un valor entre 1 y 5.\n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            seleccionarRuta();
+            
+        }
 
-    cout << "Has seleccionado la ruta: " << listaRutas[opcion - 1] << endl << endl;
+    
 }
 
-void seleccionarSentido() {
+void seleccionarSentido() { // función para seleccionar el sentido de la ruta, si es de ida o vuelta
     cout << "1. IDA\n" << "2. VUELTA\n" << "SELECCIONE EL SENTIDO: ";
     cin >> sentido;
+
+    if (sentido < 1 || sentido > 2) {
+        cout << "Opcion invalida. Por favor seleccione 1 o 2." << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        seleccionarSentido();
+    }
 }
 
-void seleccionarParada() {
+void seleccionarParada() { // función para saber en que parada se bajará el usuario
     cout << "Selecciona en que escalon te bajaras: ";
     cin >> parada;
 
 }
 
-bool verificarParadaEnArchivo(const string& archivo_ruta, const string& paradaBuscada) {
+bool verificarParadaEnArchivo(const string& archivo_ruta, const string& paradaBuscada) { // función para verificar si la parada que ingresó el usuario existe en el archivo de la ruta
     ifstream archivo(archivo_ruta);
     if (!archivo.is_open()) {
         cerr << "Error: no se pudo abrir el archivo " << archivo_ruta << endl;
         return false;
     }
 
-    string linea;
+    
     int numeroLinea = 0; // para saber en qué línea está la parada
     bool encontrada = false;
 
@@ -72,9 +93,9 @@ bool verificarParadaEnArchivo(const string& archivo_ruta, const string& paradaBu
         while (!linea.empty() && isspace(linea.front())) linea.erase(linea.begin());
         while (!linea.empty() && isspace(linea.back())) linea.pop_back();
 
-        if (linea.empty()) continue; // ignora líneas vacías
+        if (linea.empty()) continue; // esto es para ignorar las líneas vacías
 
-        // intentamos comparar como número si es posible
+        // para intentar comparar como número si es posible
         try {
             int valorArchivo = stoi(linea);
             int valorUsuario = stoi(paradaBuscada);
@@ -85,7 +106,7 @@ bool verificarParadaEnArchivo(const string& archivo_ruta, const string& paradaBu
                 break;
             }
         } catch (...) {
-            // si no es número, comparamos texto
+            // si no es número, comparamos como texto
             if (linea == paradaBuscada) {
                 cout << "\n Parada seleccionada: " << linea << "\n" << endl;
                 encontrada = true;
@@ -97,7 +118,6 @@ bool verificarParadaEnArchivo(const string& archivo_ruta, const string& paradaBu
     archivo.close();
 
     if (!encontrada) {
-        cout << "\n La parada '" << paradaBuscada << "' no se encontró en este archivo.\n";
         return false;
     }
 
@@ -105,7 +125,7 @@ bool verificarParadaEnArchivo(const string& archivo_ruta, const string& paradaBu
 }
 
 
-void procesarRuta(const string& codigoRuta) {
+void procesarRuta(const string& codigoRuta) { // función para procesar la ruta seleccionada, el sentido y mostrar las paradas
     seleccionarSentido();
 
     string direccion = (sentido == 1) ? "Ida" : "Vuelta";
@@ -129,31 +149,56 @@ void procesarRuta(const string& codigoRuta) {
 
     if (!verificarParadaEnArchivo(archivo_ruta, parada)) {
         cout << "\n La parada '" << parada << "' no existe en esta ruta.\n";
+        procesarRuta(codigoRuta);
     }
 }
 
-void confirmarSolicitud() {
-    char confirmacion;
-    cout << "===== CONFIRMACION DE SOLICITUD =====\n" << endl;
+void nuevaOpcion() { // función en caso de que el usuario quiera reiniciar el proceso o salir de la app
+    cout << "Que desea hacer?\n" << "1. Reiniciar el proceso.\n" << "2. Salir de EscalApp.\n";    
+    cin >> nueva_opcion;    
+}
+
+void visualizarSolicitud() { // función para mostrar un resumen de la solicitud del usuario y pueda ver si está correcta
+    cout << "===== VISUALIZACION DE SOLICITUD =====\n" << endl;
     cout << "Ruta seleccionada: " << listaRutas[opcion - 1] << endl;
     cout << "Sentido: " << ((sentido == 1) ? "IDA" : "VUELTA") << endl;
-    cout << "Parada seleccionada: " << parada << endl;
+    cout << "Parada seleccionada: " << linea << endl;
+}
+
+void confirmarSolicitud() { // función para que el usuario confirme o cancele la solicitud
+    char confirmacion;
     cout << "Ingrese 'C' para confirmar o 'N' para cancelar: ";
     cin >> confirmacion;
 
     switch (confirmacion) {
         case 'C':
         case 'c':
-            cout << "Solicitud confirmada y enviada a tu conductor. Gracias por usar EscalApp!" << endl;
+            cout << "\nSolicitud confirmada y enviada a tu conductor.\nGracias por usar EscalApp!\n" << endl;
             break;
         case 'N':
         case 'n':
-            cout << "Solicitud cancelada. Puede reiniciar el proceso si lo desea." << endl;
+            cout << "Solicitud cancelada." << endl;
+            nuevaOpcion();
+            switch (nueva_opcion) {
+                case 1:
+                    seleccionarRuta();
+                    procesarRuta(listaRutas[opcion - 1]);
+                    confirmarSolicitud();
+                break;
+                case 2:
+                    cout << "Gracias por usar EscalApp. Hasta luego!" << endl;
+                    break;
+                default:
+                cout << "Opcion invalida. Por favor, intente de nuevo." << endl;
+                nuevaOpcion();
+            }
             break;
         default:
-            cout << "Opcion invalida. Por favor ingrese 'C' o 'N'." << endl;
+            cout << "\nOpcion invalida. Por favor ingrese 'C' o 'N'.\n" << endl;
+            confirmarSolicitud();
             break;
     }
 }
+
 
 #endif
--- Principal/main.cpp
+++ Principal/main.cpp
@@ -3,52 +3,63 @@
 #include <conio.h>
 #include <windows.h>
 #include <sstream>
+#include <limits>
 #include "libreria.hpp"
 #include <string>
 using namespace std;
 
 int main() {
   
-  char tecla;
+    char tecla;
 
-  bienvenida();
-  cout << "Presione la tecla 's' para iniciar..." << endl;
+    bienvenida();
+    cout << "Presione la tecla 's' para iniciar..." << endl;
 
-  do {
-    tecla = getch();
-  } while (tecla != 'S' && tecla != 's');
+    do {
+      tecla = getch();
+    } while (tecla != 'S' && tecla != 's');
 
-  barraCarga();
+    barraCarga();
 
-  seleccionarRuta();
+      
+    seleccionarRuta();
+    
+
+      switch (opcion) {
+        case 1:
+            cout << "RUTA A101" << endl;
+            procesarRuta("A101");
+            break;
+        case 2:
+            cout << "RUTA A102" << endl;
+            procesarRuta("A102");
+            break;
+        case 3:
+            cout << "RUTA T102" << endl;
+            procesarRuta("T102");
+            break;
+        case 4:
+            cout << "RUTA X102" << endl;
+            procesarRuta("X102");
+            break;
+        case 5: 
+            cout << "RUTA X103" << endl;
+            procesarRuta("X103");
+            break;
+        default:
+          cout << "Opcion invalida. Ingrese un valor entre 1 y 5.\n";
+          seleccionarRuta();
+          break;
+        }
+
+  
+    visualizarSolicitud();
+    confirmarSolicitud();
+  
+  
 
 
-  switch (opcion) {
-    case 1:
-        cout << "RUTA A101" << endl;
-        procesarRuta("A101");
-        break;
-    case 2:
-        cout << "RUTA A102" << endl;
-        procesarRuta("A102");
-        break;
-    case 3:
-        cout << "RUTA T102" << endl;
-        procesarRuta("T102");
-        break;
-    case 4:
-        cout << "RUTA X102" << endl;
-        procesarRuta("X102");
-        break;
-    case 5: 
-        cout << "RUTA X103" << endl;
-        procesarRuta("X103");
-        break;
-    default:
-      cout << "Opcion invalida. Intente de nuevo.";
-    }
+  
 
-  confirmarSolicitud();
-
-  return 0;
+    return 0;
 }
